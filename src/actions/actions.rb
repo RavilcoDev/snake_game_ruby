3796f9388a63170f2adc19438f05c99c688c272f
Rafael Livise <r.livise@hotmail.com>
2022-08-26 20:35:39 -0500

    feat: [UPDATE] add change direction

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,18 +1,18 @@
 require_relative "../model/state"
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
-    next_position = calc_next_position(state, next_direction)
-    if(position_valida?(state,next_position))
+    curr_direction = state.curr_direction
+    next_position = calc_next_position(state, curr_direction)
+    if(position_is_valida?(state,next_position))
       move_snake_to(state, next_position)
     else
       end_game state
     end
   end
 
-  def self.calc_next_position(state,next_direction)
+  def self.calc_next_position(state,curr_direction)
     current_position = state.snake.positions.first
-    case next_direction
+    case curr_direction
       when Model::Direction::UP
         return Model::Coord.new(current_position.col,
           current_position.row - 1)
@@ -28,7 +28,7 @@ module Actions
     end
   end
 
-  def self.position_valida?(state,next_position)
+  def self.position_is_valida?(state,next_position)
     is_valid = next_position.col < 0 ||
       next_position.row < 0 ||
       next_position.col > state.grid.cols ||
@@ -36,10 +36,32 @@ module Actions
     return !is_valid || state.snake.positions.include?(next_position)
   end
 
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.curr_direction = direction
+    end
+    state
+  end
+
+  def self.next_direction_is_valid?(state,direction)
+    current_direction = state.curr_direction
+    up = Model::Direction::UP
+    down = Model::Direction::DOWN
+    rigth = Model::Direction::RIGTH
+    left = Model::Direction::LEFT
+
+    return !(current_direction == direction ||
+      current_direction == up && direction == down ||
+      current_direction == down && direction == up ||
+      current_direction == rigth && direction == rigth ||
+      current_direction == left && direction == left)
+  end
+
   def self.move_snake_to(state, next_position)
     state.snake.positions = [next_position] + state.snake.positions[0...-1]
     state
   end
+
 
   def self.end_game(state)
     state.game_finished = true
--- test/test_actions.rb
+++ test/test_actions.rb
@@ -3,8 +3,9 @@ require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
 class TestActions < Minitest::Test
-  def test_move_snake
-    initial_state = Model::State.new(
+
+  def setup
+    @initial_state = Model::State.new(
       Model::Snake.new([
         Model::Coord.new(1, 1),
         Model::Coord.new(1, 0)
@@ -14,7 +15,7 @@ class TestActions < Minitest::Test
       Model::Direction::DOWN,
       false
     )
-    expected_state = Model::State.new(
+    @expected_state_secuence = Model::State.new(
       Model::Snake.new([
         Model::Coord.new(1, 2),
         Model::Coord.new(1, 1)
@@ -24,31 +25,28 @@ class TestActions < Minitest::Test
       Model::Direction::DOWN,
       false
     )
+  end
+
+  def test_move_snake
     next_position = Model::Coord.new(1, 2)
-    new_state = Actions::move_snake_to(initial_state,next_position)
-    assert_equal new_state, expected_state
+    new_state = Actions::move_snake_to(@initial_state,next_position)
+    assert_equal new_state, @expected_state_secuence
   end
 
   def test_position_valida?
-    initial_state = Model::State.new(
-      Model::Snake.new([
-        Model::Coord.new(1, 1),
-        Model::Coord.new(1, 0)
-      ]),
-      Model::Food.new(4, 4),
-      Model::Grid.new(8, 12),
-      Model::Direction::DOWN,
-      false
-    )
-
     next_position = Model::Coord.new(1,2)
 
-    is_valid_correct = Actions::position_valida?(initial_state,next_position)
+    is_valid_correct = Actions::position_is_valida?(@initial_state,next_position)
     assert_equal is_valid_correct, true
 
     out_grid = Model::Coord.new(-1,2)
 
-    is_valid_grid = Actions::position_valida?(initial_state,out_grid)
+    is_valid_grid = Actions::position_is_valida?(@initial_state,out_grid)
     assert_equal is_valid_grid, false
+  end
+
+  def test_change_direction_invalid
+    current_state = Actions::change_direction(@initial_state, Model::Direction::UP)
+    assert_equal current_state, @initial_state
   end
 end
